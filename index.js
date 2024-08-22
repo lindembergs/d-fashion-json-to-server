fe7f36a8bcaa6f993410090bde08deabee334277
Lindemberg <lindembergparaiba@gmail.com>
2024-08-22 00:48:24 -0300

    alt no caminho do json

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000; // Use a porta do ambiente para deploy
+const port = process.env.PORT || 3000;
+
+// Caminho absoluto para o arquivo db.json
+const filePath = path.join(__dirname, "db.json");
 
 // Leitura do arquivo db.json
-const data = JSON.parse(fs.readFileSync("db.json", "utf-8"));
+const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
-// Endpoint para retornar o conteúdo do db.json
+// Endpoint principal para retornar o JSON
 app.get("/", (req, res) => {
   res.json(data);
 });
 
-// Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
